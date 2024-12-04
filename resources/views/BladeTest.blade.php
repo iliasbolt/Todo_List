0e05174f6acb135e0f2ed9832cdadd4410efed57
HP <balhi.ilias@gmail.com>
2024-12-04 21:28:20 +0100

    change 1

--- resources/views/BladeTest.blade.php
+++ resources/views/BladeTest.blade.php
@@ -3,6 +3,6 @@
 @endisset
 
 @empty($name)
-    <p>Weirde u dont have a name ? Please type it  <a href="{{url('/profile')}}">here</a></p>
+    <p>Weirde u d dont have a name ? Please type it  <a href="{{url('/profile')}}">here</a></p>
 @endempty
 
